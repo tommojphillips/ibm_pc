b20e3905bb45d225dc589b774a9cc205c54266cf
Tommo J. Armytage <tommojphillips@gmail.com>
2025-09-22 15:44:16 +1000

    # refactored memory_map

--- src/backend/io/memory_map.c
+++ src/backend/io/memory_map.c
@@ -5,6 +5,7 @@
 
 #include <stdint.h>
 #include <malloc.h>
+#include <memory.h>
 
 #include "memory_map.h"
 
@@ -16,132 +17,209 @@
 #define dbg_print(x, ...)
 #endif
 
-int create_memory_map(MEMORY_MAP* map, int memory_buffer_size) {
-	
-	(*map).regions = (MEMORY_REGION*)calloc(10, sizeof(MEMORY_REGION));
-	if ((*map).regions == NULL) {
-		return 1;
-	}
+/* Mregion Start */
+#define MR_START     (map->regions[i].start)
 
-	(*map).internal_buffer_size = 2;
-	(*map).internal_buffer = (uint8_t*)calloc(1, (*map).internal_buffer_size);
-	if ((*map).internal_buffer == NULL) {
-		return 1;
-	}
+/* Mregion Size */
+#define MR_SIZE      (map->regions[i].size)
 
-	(*map).region_count = 10;
-	(*map).region_index = 0;
+/* Mregion Mask */
+#define MR_MASK      (map->regions[i].mask)
 
-	(*map).mem_size = memory_buffer_size;
-	(*map).mem = (uint8_t*)calloc(1, memory_buffer_size);
-	if ((*map).mem == NULL) {
-		return 1;
-	}
+/* Mregion End */
+#define MR_END       (MR_START + MR_SIZE)
 
-	return 0;
-}
-void destroy_memory_map(MEMORY_MAP* map) {
+/* Mregion Pointer */
+#define MR_PTR       (map->mem + MR_START + ((address - MR_START) & MR_MASK))
+
+/* Is Mregion write protected */
+#define IS_WRITE_PROTECTED(i) (map->regions[i].flags & MREGION_FLAG_WRITE_PROTECTED)
+
+/* Is Mregion not write protected */
+#define IS_WRITABLE(i) (!IS_WRITE_PROTECTED(i))
+
+/* Is Mregion removed */
+#define IS_REMOVED(i) (map->regions[i].flags & MREGION_FLAG_REMOVED)
+
+/* Is Mregion enabled */
+#define IS_ENABLED(i) (map->regions[i].flags & MREGION_FLAG_ENABLED)
+
+/* Is Mregion active */
+#define IS_ACTIVE(i)  (!IS_REMOVED(i) && IS_ENABLED(i))
+
+/* Is index in range of start, end */
+#define IS_IN_RANGE(i, start, end) ((i) >= (start) && (i) < (end))
+
+/* --- Memory Map --- */
+int memory_map_create(MEMORY_MAP* map, uint32_t buffer_size, int region_count) {
 	if (map != NULL) {
-		
+		/* alloc mregions */
+		map->regions = (MEMORY_REGION*)calloc(region_count, sizeof(MEMORY_REGION));
+		if (map->regions == NULL) {
+			dbg_print("Failed to create memory map; Calloc failed. region_count = %x, region_size = %zu\n", region_count, sizeof(MEMORY_REGION));
+			return 1;
+		}
+		map->region_count = region_count;
+		map->region_index = 0;
+
+		/* alloc memory buffer */
+		map->mem = (uint8_t*)calloc(1, buffer_size);
+		if (map->mem == NULL) {
+			dbg_print("Failed to create memory map; Calloc failed. buffer_size = %x\n", buffer_size);
+			return 1;
+		}
+		map->mem_size = buffer_size;
+
+		return 0;
+	}
+	dbg_print("Failed to create memory map; Map was NULL.\n");
+	return 1;
+}
+void memory_map_destroy(MEMORY_MAP* map) {
+	if (map != NULL) {
+		/* free mregions */		
 		if (map->regions != NULL) {
 			free(map->regions);
 			map->regions = NULL;
 		}
+		map->region_count = 0;
+		map->region_index = 0;
 
+		/* free memory buffer */		
 		if (map->mem != NULL) {
 			free(map->mem);
 			map->mem = NULL;
 			map->mem_size = 0;
 		}
-
-		if (map->internal_buffer != NULL) {
-			free(map->internal_buffer);
-			map->internal_buffer = NULL;
-			map->internal_buffer_size = 0;
-		}
-		
-		map->region_count = 0;
-		map->region_index = 0;
 	}
 }
 
-int add_memory_region(MEMORY_MAP* map, uint32_t start, uint32_t size, uint32_t mask, int flags) {
-
-	int index = -1;
+uint8_t memory_map_read_byte(MEMORY_MAP* map, uint32_t address) {
+	
+	/* Handle mregion */
 	for (int i = 0; i < map->region_index; ++i) {
-		if (map->regions[i].flags & MREGION_FLAG_REMOVED) {
+		if (IS_ACTIVE(i) && IS_IN_RANGE(address, MR_START, MR_END)) {
+			return *(uint8_t*)(map->mem + MR_START + ((address - MR_START) & map->regions[i].mask));
+		}
+	}
+
+	dbg_print("reading from %x\n", address);
+	return 0;
+}
+void memory_map_write_byte(MEMORY_MAP* map, uint32_t address, uint8_t value) {
+
+	/* Handle mregion */
+	for (int i = 0; i < map->region_index; ++i) {
+		if (IS_ACTIVE(i) && IS_WRITABLE(i) && IS_IN_RANGE(address, MR_START, MR_END)) {
+			*(uint8_t*)(map->mem + MR_START + ((address - MR_START) & map->regions[i].mask)) = value;
+			return;
+		}
+	}
+
+	dbg_print("writing %x to %x\n", value, address);
+}
+void memory_map_set_writeable_region(MEMORY_MAP* map, uint8_t value) {
+	for (int i = 0; i < map->region_index; ++i) {
+		if (IS_ACTIVE(i) && IS_WRITABLE(i)) {
+			for (uint32_t j = 0; j < MR_SIZE; ++j) {
+				memory_map_write_byte(map, MR_START + j, value);
+			}
+			//dbg_print("mregion set %x - %x = %x\n", MR_START, map->regions[i].end, value);
+		}
+	}
+}
+
+int memory_map_validate(MEMORY_MAP* map) {
+	if (!map || !map->regions) {
+		dbg_print("Memory map not initialized.\n");
+		return 1;
+	}
+
+	for (int i = 0; i < map->region_index; ++i) {
+		if (IS_ACTIVE(i)) {
+
+			uint32_t start = MR_START;
+			uint32_t size = MR_SIZE;
+			uint32_t end = MR_END;
+			uint32_t mask = MR_MASK;
+
+			if (size == 0) {
+				dbg_print("Memory Map warning: region[%d] has zero size.\n", i);
+			}			
+
+			if (mask == 0) {
+				dbg_print("Memory Map warning: region[%d] has zero mask.\n", i);
+			}
+
+			/* Overlap check */
+			for (int j = 0; j < map->region_index; ++j) {
+				if (IS_ACTIVE(j) && j != i) {
+
+					uint32_t other_start = map->regions[j].start;
+					uint32_t other_end = other_start + map->regions[j].size;
+
+					if (!(end <= other_start || start >= other_end)) {
+						dbg_print("Memory Map warning: region[%d] (%x-%x) overlaps region[%d] (%x-%x).\n", i, start, end - 1, j, other_start, other_end - 1);
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
+
+/* --- Memory Region --- */
+
+int memory_map_add_mregion(MEMORY_MAP* map, uint32_t start, uint32_t size, uint32_t mask, uint32_t flags) {
+	int index = -1;
+
+	/* find first removed mregion */
+	for (int i = 0; i < map->region_index; ++i) {
+		if (IS_REMOVED(i)) {
 			index = i;
 		}
 	}
 
+	/* did not find a removed mregion; add to end */
 	if (index < 0) {
 		index = map->region_index;
-		map->region_index++;
-		if (index >= map->region_count - 1) {
-			void* regions = realloc(map->regions, (size_t)((map->region_count + 10) * sizeof(MEMORY_REGION)));
-			if (regions == NULL) {
-				return -1;
-			}
-			map->regions = regions;
-			map->region_count += 10;
+		if (index >= map->region_count) {
+			dbg_print("Failed to add mregion; Index out of range. start = %x, size = %x, mask = %x, flags = %x\n", start, size, mask, flags);
+			return -1;
 		}
+		map->region_index++;
 	}
 
 	map->regions[index].start = start;
 	map->regions[index].size = size;
 	map->regions[index].mask = mask;
-	map->regions[index].flags = flags;
+	map->regions[index].flags = flags | MREGION_FLAG_ENABLED;
 	return index;
 }
-int remove_memory_region(MEMORY_MAP* map, int index) {
-	if (index < map->region_count) {
+int memory_map_remove_mregion(MEMORY_MAP* map, int index) {
+	if (IS_IN_RANGE(index, 0, map->region_index)) {
 		map->regions[index].start = 0;
 		map->regions[index].size = 0;
+		map->regions[index].mask = 0;
 		map->regions[index].flags = MREGION_FLAG_REMOVED;
-
-		if (index == map->region_index - 1) {
-			map->region_index--;
-		}
+		return 0;
 	}
-	else {
-		return 1;
-	}
-	return 0;
+	dbg_print("Failed to remove mregion; Index out of range. index = %d\n", index);
+	return 1;
 }
-
-#define B_START     map->regions[i].start
-#define B_SIZE      map->regions[i].size
-#define B_END       (B_START + B_SIZE)
-#define B_PTR       (map->mem + B_START + ((address - B_START) & map->regions[i].mask))
-#define IS_WRITABLE (!(map->regions[i].flags & MREGION_FLAG_WRITE_PROTECTED))
-
-uint8_t memory_map_read_byte(MEMORY_MAP* map, uint32_t address) {
-	for (int i = 0; i < map->region_index; ++i) {
-		if (address < B_END && address >= B_START) {
-			return *(uint8_t*)B_PTR;
-		}
+int memory_map_enable_mregion(MEMORY_MAP* map, int index) {
+	if (IS_IN_RANGE(index, 0, map->region_index) && !IS_REMOVED(index)) {
+		map->regions[index].flags |= MREGION_FLAG_ENABLED;
+		return 0;
 	}
-	dbg_print("reading from %x\n", address);
-	return 0;
+	dbg_print("Failed to enable mregion; Index out of range or mregion removed. index = %d, removed = %x\n", index, IS_REMOVED(index));
+	return 1;
 }
-void memory_map_write_byte(MEMORY_MAP* map, uint32_t address, uint8_t value) {
-	for (int i = 0; i < map->region_index; ++i) {
-		if (address < B_END && address >= B_START) {
-			if (IS_WRITABLE) {
-				*(uint8_t*)B_PTR = value;
-			}
-			return;
-		}
+int memory_map_disable_mregion(MEMORY_MAP* map, int index) {
+	if (IS_IN_RANGE(index, 0, map->region_index) && !IS_REMOVED(index)) {
+		map->regions[index].flags &= ~MREGION_FLAG_ENABLED;
+		return 0;
 	}
-	dbg_print("writing %x to %x\n", value, address);
-}
-
-void* __cdecl memset(void* _Dst, int _Val, size_t _Size);
-
-void memory_map_set_writeable_region(MEMORY_MAP* map, uint8_t value) {
-	for (int i = 0; i < map->region_index; ++i) {
-		if (IS_WRITABLE) {
-			memset(map->mem + B_START, value, B_SIZE);
-		}
-	}
+	dbg_print("Failed to disable mregion; Index out of range or mregion removed. index = %d, removed = %x\n", index, IS_REMOVED(index));
+	return 1;
 }
--- src/backend/io/memory_map.h
+++ src/backend/io/memory_map.h
@@ -8,66 +8,96 @@
 
 #include <stdint.h>
 
-#define MREGION_FLAG_NONE            0
-#define MREGION_FLAG_WRITE_PROTECTED 1
-#define MREGION_FLAG_MIRRORED        4
-#define MREGION_FLAG_REMOVED         8
+typedef struct MEMORY_REGION MEMORY_REGION;
 
-#define USE_REGIONS 1
+ /* --- Memory Map --- */
 
-typedef struct {
-	uint32_t start;
-	uint32_t size;
-	uint32_t mask;
-	int flags;
-} MEMORY_REGION;
-
-typedef struct {
+/* Memory Map */
+typedef struct MEMORY_MAP {
 	MEMORY_REGION* regions;
 	int region_count;
 	int region_index;
 	uint8_t* mem;
-	int mem_size;
-	uint8_t* internal_buffer;
-	int internal_buffer_size;
+	uint32_t mem_size;
 } MEMORY_MAP;
 
-/* Create memory map 
-	map:	 (out) points to the created map
-	Returns: 1 if error or 0 if success */
-int create_memory_map(MEMORY_MAP* map, int memory_buffer_size);
+/* Creates a memory map; allocates memory for the mregions, memory buffer
+	map:	      the map instance
+	buffer_size:  the size of the memory buffer
+	region_count: the number of memory regions in the memory map
+	Returns:      1 if error or 0 if success */
+int memory_map_create(MEMORY_MAP* map, uint32_t buffer_size, int region_count);
 
-/* Destroy memory map */
-void destroy_memory_map(MEMORY_MAP* map);
+/* Destroys a memory map
+	map: the map instance */
+void memory_map_destroy(MEMORY_MAP* map);
 
-/* Add a memory region to the memory map.
-   map:     the map to add a region to
-   start:   the region start
-   size:    the region size
-   mask:    the region address mask
-   flags:   the region flags
-   Returns: -1 if error or the index of the added memory region on success */
-int add_memory_region(MEMORY_MAP* map, uint32_t start, uint32_t size, uint32_t mask, int flags);
-
-/* Remove a memory region to the memory map. 
-   map:     the map to add a region to
-   index:   the region index
-   Returns: 1 if error or 0 on success */
-int remove_memory_region(MEMORY_MAP* map, int index);
-
-/* Read byte from memory region */
+/* Read byte from memory map
+	map:     the map instance
+	address: the address to read from
+	Returns: the value that was read at the address */
 uint8_t memory_map_read_byte(MEMORY_MAP* map, uint32_t address);
 
-/* Write byte to memory region */
+/* Write byte to memory map
+	map:     the map instance
+	address: the address to write to
+	value:   the value to write at the address */
 void memory_map_write_byte(MEMORY_MAP* map, uint32_t address, uint8_t value);
 
-/* Read word from memory region */
-uint16_t memory_map_read_word(MEMORY_MAP* map, uint32_t address);
-
-/* Write word to memory region */
-void memory_map_write_word(MEMORY_MAP* map, uint32_t address, uint16_t value);
-
-/* Set all writable regions to value */
+/* Set all writable memory regions to value
+	map:     the map instance
+	value:   the value to write */
 void memory_map_set_writeable_region(MEMORY_MAP* map, uint8_t value);
 
-#endif
+int memory_map_validate(MEMORY_MAP* map);
+
+/* --- Memory Region --- */
+
+/* Memory region has no flags */
+#define MREGION_FLAG_NONE            0x00
+
+/* Memory region is write protected */
+#define MREGION_FLAG_WRITE_PROTECTED 0x01
+
+/* Memory region is enabled */
+#define MREGION_FLAG_ENABLED         0x02
+
+ /* Memory region has been removed; A new mregion can override this mregion */
+#define MREGION_FLAG_REMOVED         0x04
+
+/* Memory Region */
+typedef struct MEMORY_REGION {
+	uint32_t start; /* the mregion start address */
+	uint32_t size;  /* the mregion size */
+	uint32_t mask;  /* the mregion address mask */
+	uint32_t flags; /* flags for the mregion (MREGION_FLAG_XXX) */
+} MEMORY_REGION;
+
+/* Add a mregion to the memory map and enables it
+	map:     the map instance
+	start:   the region start offset
+	size:    the region size
+	mask:    the region address mask
+	flags:   the region flags
+	Returns: -1 if error or the index of the added mregion on success */
+int memory_map_add_mregion(MEMORY_MAP* map, uint32_t start, uint32_t size, uint32_t mask, uint32_t flags);
+
+/* Remove a mregion from the memory map
+	map:     the map instance
+	index:   the region index
+	Returns: 1 if error or 0 on success */
+int memory_map_remove_mregion(MEMORY_MAP* map, int index);
+
+/* Enable a mregion in the memory map
+	map:     the map instance
+	index:   the region index
+	Returns: 1 if error or 0 on success */
+int memory_map_enable_mregion(MEMORY_MAP* map, int index);
+
+/* Disable a mregion in the memory map
+	map:     the map instance
+	index:   the region index
+	Returns: 1 if error or 0 on success */
+int memory_map_disable_mregion(MEMORY_MAP* map, int index);
+
+#endif /* MEMORY_MAP_H */
