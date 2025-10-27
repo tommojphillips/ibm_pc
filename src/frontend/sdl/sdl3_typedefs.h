2e00f47ae862a04119d1922e5a236b3470817be2
Tommo J. Armytage <tommojphillips@gmail.com>
2025-10-27 19:03:40 +1000

    # added struct COLOR_RGB to sdl3_typedefs.h

--- src/frontend/sdl/sdl3_typedefs.h
+++ src/frontend/sdl/sdl3_typedefs.h
@@ -1,5 +1,5 @@
 /* graphics_typedefs.h
-* GitHub: https:\\github.com\tommojphillips
+ * Thomas J. Armytage 2025 ( https://github.com/tommojphillips/ )
 */
 
 #ifndef GRAPHICS_TYPEDEFS_H
@@ -13,5 +13,11 @@ typedef struct COLOR_RGBA {
 	uint8_t b;
 	uint8_t a;
 } COLOR_RGBA;
+
+typedef struct COLOR_RGB {
+	uint8_t r;
+	uint8_t g;
+	uint8_t b;
+} COLOR_RGB;
 
 #endif
