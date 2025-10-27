4c3bd3068e6b7055d042e46789dbcaffb831d409
Tommo J. Armytage <tommojphillips@gmail.com>
2025-10-27 19:06:46 +1000

    # refactored dbg_gui.c # added AVG FPS counters

--- src/frontend/sdl/dbg_gui.c
+++ src/frontend/sdl/dbg_gui.c
@@ -4,7 +4,6 @@
  */
 
 #include <stdint.h>
-#include <stdio.h>
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_init.h>
@@ -17,14 +16,18 @@
 
 #include "dbg_gui.h"
 
-#include "../../backend/ibm_pc.h"
+#include "backend/ibm_pc.h"
 
 #define DBG_PRINT
 #ifdef DBG_PRINT
+#include <stdio.h>
 #define dbg_print(x, ...) printf(x, __VA_ARGS__)
 #else
 #define dbg_print(x, ...)
 #endif
+
+ /* Get 20bit address SEG:ADDR */
+#define PHYS_ADDRESS(seg, offset) ((((uint20_t)seg << 4) + ((offset) & 0xFFFF)) & 0xFFFFF)
 
 enum {
 	IRQ_TIMER0 = 0x00,
@@ -44,32 +47,49 @@ enum {
 	IRQ_ATC2   = 0x0F  // MODEL_5170 *can* use IRQ 15 for secondary ATC controller interrupts
 };
 
+void avg_frame_timer_add(AVG_FRAME_TIMER* t, double ms) {
+	// Subtract the old value from the sum
+	t->sum -= t->frame_times[t->index];
+
+	// Add the new value
+	t->frame_times[t->index] = ms;
+	t->sum += ms;
+
+	// Advance the index
+	t->index = (t->index + 1) % FRAME_HISTORY;
+
+	// Track number of valid frames
+	if (t->count < FRAME_HISTORY) t->count++;
+}
+double avg_frame_timer_get(const AVG_FRAME_TIMER* t) {
+	return (t->count > 0) ? (t->sum / t->count) : 0.0;
+}
+
 void print_timer(int i, I8253_TIMER* timer, char* str);
 
 static void print_video_adapter(WINDOW_INSTANCE* instance, DBG_GUI* gui, float x, float y) {
 	// print display adapter and mode
-	if (ibm_pc->video_adapter == VIDEO_ADAPTER_MDA) {
-		sprintf(gui->str, "MDA 80x25");
+	if (ibm_pc->config.video_adapter == VIDEO_ADAPTER_MDA_80X25) {
+		sprintf(gui->str, "MDA %dx%d", ibm_pc->mda.crtc.hdisp, ibm_pc->mda.crtc.vdisp);
 	}
-	else if (ibm_pc->video_adapter == VIDEO_ADAPTER_CGA) {
+	else if (ibm_pc->config.video_adapter == VIDEO_ADAPTER_CGA_80X25 || ibm_pc->config.video_adapter == VIDEO_ADAPTER_CGA_40X25) {
 		if (ibm_pc->cga.mode & CGA_MODE_GRAPHICS) {
-			if (ibm_pc->cga.mode & CGA_MODE_GRAPHICS_RES_HI) {
-				sprintf(gui->str, "CGA Graphics 640x200");
-			}
-			else {
-				sprintf(gui->str, "CGA Graphics 320x200");
-			}
+			sprintf(gui->str, "CGA Graphics %dx%d", ibm_pc->cga.width, ibm_pc->cga.height);
 		}
 		else {
-			if (ibm_pc->cga.mode & CGA_MODE_TEXT_RES_HI) {
-				sprintf(gui->str, "CGA 80x25");
-			}
-			else {
-				sprintf(gui->str, "CGA 40x25");
-			}
+			sprintf(gui->str, "CGA %dx%d", ibm_pc->cga.crtc.hdisp, ibm_pc->cga.crtc.vdisp);
 		}
 	}
-	sprintf(gui->str + strlen(gui->str), " @ %.0fhz", 1000.0 / gui->win->time.last_ms);
+	else if (ibm_pc->config.video_adapter == VIDEO_ADAPTER_NONE) {
+		sprintf(gui->str, "HEADLESS");
+	}
+
+	// get avg fps
+	avg_frame_timer_add(&gui->win_avg_fps, gui->win->time.last_ms);
+
+	if (gui->win != NULL) {
+		sprintf(gui->str + strlen(gui->str), " @ %.2fhz", HZ_TO_MS(avg_frame_timer_get(&gui->win_avg_fps)));
+	}
 	SDL_RenderDebugText(instance->renderer, x, y, gui->str);
 }
 void dbg_gui_render(WINDOW_INSTANCE* instance, DBG_GUI* gui) {
@@ -77,14 +97,29 @@ void dbg_gui_render(WINDOW_INSTANCE* instance, DBG_GUI* gui) {
 	// update stats
 	SDL_SetRenderDrawColor(instance->renderer, 0xFF, 0, 0, 0xFF);
 	
+	// get avg fps
+	avg_frame_timer_add(&gui->emu_avg_fps, ibm_pc->time.last_ms);
+
 	// print cpu/pit info	
+	
+	sprintf(gui->str, "CGA %6llu cycles", ibm_pc->cga_cycles);
+	SDL_RenderDebugText(instance->renderer, 10.0f, instance->transform.h - 80.0f, gui->str);
+	
+	sprintf(gui->str, "KBD %6llu cycles", ibm_pc->kbd_cycles);
+	SDL_RenderDebugText(instance->renderer, 10.0f, instance->transform.h - 70.0f, gui->str);
+	
+	sprintf(gui->str, "DMA %6llu cycles", ibm_pc->dma_cycles);
+	SDL_RenderDebugText(instance->renderer, 10.0f, instance->transform.h - 60.0f, gui->str);
+	
+	sprintf(gui->str, "FDC %6llu cycles", ibm_pc->fdc_cycles);
+	SDL_RenderDebugText(instance->renderer, 10.0f, instance->transform.h - 50.0f, gui->str);
 	
 	sprintf(gui->str, "PIT %6llu cycles", ibm_pc->pit_cycles);
 	SDL_RenderDebugText(instance->renderer, 10.0f, instance->transform.h - 40.0f, gui->str);
 	
-	sprintf(gui->str, "CPU %6llu cycles @ %.0fhz", ibm_pc->cpu_cycles, 1000.0 / ibm_pc->time.last_ms);
+	sprintf(gui->str, "CPU %6llu cycles @ %.2fhz", ibm_pc->cpu_cycles, HZ_TO_MS(avg_frame_timer_get(&gui->emu_avg_fps)));
 	SDL_RenderDebugText(instance->renderer, 10.0f, instance->transform.h - 30.0f, gui->str);
-	
+		
 	print_video_adapter(instance, gui, 10.0f, (instance->transform.h - 10.0f));
 	
 	float h = 10;
@@ -92,17 +127,17 @@ void dbg_gui_render(WINDOW_INSTANCE* instance, DBG_GUI* gui) {
 	// print cpu registers/instuction
 	uint16_t ip = ibm_pc->cpu.ip;
 	for (int i = 0; i < 5; ++i) {
-		i8086_mnem_at(ibm_pc->mnem, ibm_pc->cpu.segments[SEG_CS], ip);
-		sprintf(gui->str, "%04X.%04X: %s", ibm_pc->mnem->segment, ip, ibm_pc->mnem->str);
+		i8086_mnem_at(&ibm_pc->mnem, ibm_pc->cpu.segments[SEG_CS], ip);
+		sprintf(gui->str, "%04X.%04X: %s", ibm_pc->mnem.segment, ip, ibm_pc->mnem.str);
 		SDL_RenderDebugText(instance->renderer, 10.0f, h, gui->str);
 		
-		for (int j = 0; j < ibm_pc->mnem->counter; ++j) {
-			sprintf(gui->str + (j * 3), " %02X", ibm_pc->cpu.funcs.read_mem_byte((ibm_pc->cpu.segments[SEG_CS] << 4) + ip + j));
+		for (int j = 0; j < ibm_pc->mnem.counter; ++j) {
+			sprintf(gui->str + (j * 3), " %02X", ibm_pc->cpu.funcs.read_mem_byte(PHYS_ADDRESS(ibm_pc->cpu.segments[SEG_CS], ip + j)));
 		}
 		SDL_RenderDebugText(instance->renderer, 280.0f, h, gui->str);
 		
 		h += 10;
-		ip += ibm_pc->mnem->counter;
+		ip += ibm_pc->mnem.counter;
 	}
 
 	h += 5;
@@ -228,12 +263,12 @@ void dbg_gui_render(WINDOW_INSTANCE* instance, DBG_GUI* gui) {
 
 	float tmp2_h = h;
 	h = tmp_h;
-	if (ibm_pc->cpu.pins.intr) {
+	if (ibm_pc->cpu.intr) {
 		sprintf(gui->str, "INTR ");
 		SDL_RenderDebugText(instance->renderer, 50.0f, h, gui->str);
 		h += 10;
 	}
-	if (ibm_pc->cpu.pins.nmi) {
+	if (ibm_pc->cpu.nmi) {
 		sprintf(gui->str, "NMI ");
 		SDL_RenderDebugText(instance->renderer, 50.0f, h, gui->str);
 		h += 10;
@@ -245,11 +280,12 @@ void dbg_gui_render(WINDOW_INSTANCE* instance, DBG_GUI* gui) {
 	
 	h += 5;	
 
-#if 0
-	int i = sdl->key_state.head;
-	for (int c = 0; c < sdl->key_state.count; ++c) {
-		sprintf(gui->str, "(%x) KEY: %02X", i, sdl->key_state.buffer[i]);
-		i = (i + 1) % KEYS_SIZE;
+#if 1
+	int i = 0;
+	uint8_t c = 0;
+	while (ring_buffer_peek(&sdl->key_state, i, &c) == 0) {
+		sprintf(gui->str, "(%x) KEY: %02X", i, c);
+		i++;
 		SDL_RenderDebugText(instance->renderer, 10.0f, h, gui->str);
 		h += 10;
 	}
@@ -257,12 +293,7 @@ void dbg_gui_render(WINDOW_INSTANCE* instance, DBG_GUI* gui) {
 	h += 5;
 	sprintf(gui->str, "Tail: %d, Head: %d, Count: %d ", sdl->key_state.tail, sdl->key_state.head, sdl->key_state.count);
 	SDL_RenderDebugText(instance->renderer, 10.0f, h, gui->str); 
-	h += 10;
-
-	for (int t = 0; t < sdl->key_state.count; ++t) {
-		SDL_RenderDebugText(instance->renderer, 50.0f, h, gui->str);
-		h += 10;
-	}
+	h += 15;
 #endif
 
 	for (int t = 0; t < 3; ++t) {
@@ -274,12 +305,16 @@ void dbg_gui_render(WINDOW_INSTANCE* instance, DBG_GUI* gui) {
 
 void print_timer(int i, I8253_TIMER* timer, char* str) {
 	
+	double clk = (double)PIT_CLOCK;
 	double freq = 0;
 	if (timer->reload != 0) {
-		freq = 1193182.0 / timer->reload;
+		freq = clk / timer->reload;
+	}
+	else {
+		freq = clk / 0x10000;
 	}
 
-	sprintf(str, "Timer%d: %04X %05X %.2fhz - ", i, timer->counter, timer->reload, freq);
+	sprintf(str, "Timer%d: %04X %04X %.2fhz - ", i, timer->counter, timer->reload, freq);
 	switch (timer->channel_state) {
 		case I8253_TIMER_STATE_COUNTING:
 			strcat(str, "Counting");
--- src/frontend/sdl/dbg_gui.h
+++ src/frontend/sdl/dbg_gui.h
@@ -1,5 +1,5 @@
 /* dbg_gui.h
-* GitHub: https:\\github.com\tommojphillips
+ * Thomas J. Armytage 2025 ( https://github.com/tommojphillips/ )
 */
 
 #ifndef DBG_GUI_H
@@ -9,9 +9,19 @@
 
 typedef struct WINDOW_INSTANCE WINDOW_INSTANCE;
 
+#define FRAME_HISTORY 60
+typedef struct {
+	double frame_times[FRAME_HISTORY]; // ms per frame
+	int index;                         // current write position
+	int count;                         // number of frames stored (<= FRAME_HISTORY)
+	double sum;                        // rolling sum of frame times
+} AVG_FRAME_TIMER;
+
 typedef struct DBG_GUI {
 	WINDOW_INSTANCE* win;
 	char str[64];
+	AVG_FRAME_TIMER emu_avg_fps;
+	AVG_FRAME_TIMER win_avg_fps;
 } DBG_GUI;
 
 extern void dbg_gui_render(WINDOW_INSTANCE* instance, DBG_GUI* gui);
