bb47d8a5e68df6d4094570c15d5e41e581cdd194
Tommo J. Armytage <tommojphillips@gmail.com>
2025-10-27 19:05:27 +1000

    # refactored sdl3_common.c # SDL state can now have multiple on_update() funcs and on_process_event() funcs

--- src/frontend/sdl/sdl3_common.c
+++ src/frontend/sdl/sdl3_common.c
@@ -1,5 +1,5 @@
 /* sdl3_common.c
-* GitHub: https:\\github.com\tommojphillips
+ * Thomas J. Armytage 2025 ( https://github.com/tommojphillips/ )
 */
 
 #include <SDL3/SDL.h>
@@ -8,7 +8,6 @@
 #include <SDL3_ttf/SDL_ttf.h>
 
 #include "sdl3_common.h"
-#include "sdl3_window.h"
 
 #define DBG_PRINT
 #ifdef DBG_PRINT
@@ -21,14 +20,6 @@
 /* Global SDL State */
 SDL* sdl = NULL;
 
-static void process_event(SDL_Event* e) {
-	switch (e->type) {
-		case SDL_EVENT_QUIT:
-			sdl->quit = 1;
-			break;
-	}
-}
-
 /* SDL struct */
 int sdl_create(void) {
 	if (sdl != NULL) {
@@ -36,13 +27,13 @@ int sdl_create(void) {
 		return 1;
 	}
 
-	sdl = (SDL*)calloc(1, sizeof(SDL));
+	sdl = calloc(1, sizeof(SDL));
 	if (sdl == NULL) {
 		dbg_print("Failed to allocate Global SDL instance\n");
 		return 1;
 	}
 
-	if (ring_buffer_init(&sdl->key_state, KEYS_SIZE)) {
+	if (ring_buffer_create(&sdl->key_state, KEYS_SIZE)) {
 		dbg_print("Failed to allocate key state ring buffer\n");
 		return 1;
 	}
@@ -60,13 +51,19 @@ int sdl_create(void) {
 	}
 
 	sdl->quit = 0;
+
+	sdl->on_process_event = NULL;
+	sdl->on_process_event_count = 0;
+	sdl->on_process_event_index = 0;
+	
+	sdl->on_update = NULL;
+	sdl->on_update_count = 0;
+	sdl->on_update_index = 0;
 	return 0;
 }
 void sdl_destroy(void) {
 
 	if (sdl != NULL) {
-
-		window_manager_destroy();
 
 		TTF_Quit();
 		SDL_QuitSubSystem(SDL_INIT_VIDEO);
@@ -74,15 +71,76 @@ void sdl_destroy(void) {
 
 		ring_buffer_destroy(&sdl->key_state);
 
+		if (sdl->on_update != NULL) {
+			free(sdl->on_update);
+			sdl->on_update = NULL;
+		}
+		sdl->on_update_count = 0;
+		sdl->on_update_index = 0;
+
+		if (sdl->on_process_event != NULL) {
+			free(sdl->on_process_event);
+			sdl->on_process_event = NULL;
+		}
+		sdl->on_process_event_count = 0;
+		sdl->on_process_event_index = 0;
+
 		/* Free Global SDL State */
 		free(sdl);
 		sdl = NULL;
 	}
 }
+
 void sdl_update(void) {
 	while (SDL_PollEvent(&sdl->e)) {
-		process_event(&sdl->e);
-		window_manager_process_event(&sdl->e);
+		/* call on_process_event() */
+		for (int i = 0; i < sdl->on_process_event_index; ++i) {
+			if (sdl->on_process_event[i](&sdl->e)) {
+				sdl->quit = 1;
+				return;
+			}
+		}
 	}
-	window_manager_update();
+
+	/* call on_update() */
+	for (int i = 0; i < sdl->on_update_index; ++i) {
+		sdl->on_update[i]();
+	}
+}
+
+int sdl_add_cb_on_process_event(SDL_PROCESS_EVENT_CB cb) {
+	int index = sdl->on_process_event_index;
+
+	if (index >= sdl->on_process_event_count) {
+		SDL_PROCESS_EVENT_CB* new_cb = realloc(sdl->on_process_event, ((size_t)sdl->on_process_event_count + 1) * sizeof(SDL_PROCESS_EVENT_CB));
+		if (new_cb == NULL) {
+			printf("[SDL] Failed to realloc on_process_event\n");
+			return -1;
+		}
+
+		sdl->on_process_event = new_cb;
+		sdl->on_process_event_count += 1;
+	}
+
+	sdl->on_process_event[index] = cb;
+	sdl->on_process_event_index++;
+	return index;
+}
+int sdl_add_cb_on_update(SDL_UPDATE_CB cb) {
+	int index = sdl->on_update_index;
+
+	if (index >= sdl->on_update_count) {
+		SDL_UPDATE_CB* new_cb = realloc(sdl->on_update, ((size_t)sdl->on_update_count + 1) * sizeof(SDL_UPDATE_CB));
+		if (new_cb == NULL) {
+			printf("[SDL] Failed to realloc on_update\n");
+			return -1;
+		}
+
+		sdl->on_update = new_cb;
+		sdl->on_update_count += 1;
+	}
+
+	sdl->on_update[index] = cb;
+	sdl->on_update_index++;
+	return index;
 }
--- src/frontend/sdl/sdl3_common.h
+++ src/frontend/sdl/sdl3_common.h
@@ -1,5 +1,5 @@
 /* sdl3_common.h
-* GitHub: https:\\github.com\tommojphillips
+ * Thomas J. Armytage 2025 ( https://github.com/tommojphillips/ )
 */
 
 #ifndef SDL3_COMMON_H
@@ -9,9 +9,12 @@
 
 #include <SDL3/SDL.h>
 
-#include "backend\ring_buffer.h"
+#include "backend/ring_buffer.h"
 
 #define KEYS_SIZE 10
+
+typedef int(*SDL_PROCESS_EVENT_CB)(SDL_Event* e);
+typedef void(*SDL_UPDATE_CB)(void);
 
 /* Window SDL */
 typedef struct SDL {
@@ -19,21 +22,35 @@ typedef struct SDL {
 	int quit;
 	RING_BUFFER key_state;
 	int kbd_new_input;
+
+	SDL_UPDATE_CB* on_update;
+	int32_t on_update_count;
+	int32_t on_update_index;
+
+	SDL_PROCESS_EVENT_CB* on_process_event;
+	int32_t on_process_event_count;
+	int32_t on_process_event_index;
 } SDL;
 
 /* Global SDL State */
 extern SDL* sdl;
 
-/* SDL Create; Allocates memory for the SDL structure 
- Returns: 1 if error; 0 if success */
+/* SDL Create 
+ \return 1 if error; 0 if success */
 int sdl_create(void);
 
 /* SDL Destroy */
 void sdl_destroy(void);
 
-/* SDL Update; Updates all window instances from the service_list.
- Calls on_process_event() for all windows in the service_list.
- Checks if the desired frame time for every window has elapsed and calls on_render(). */
+/* SDL Update */
 void sdl_update(void);
+
+/* SDL Add on_process_event Callback 
+ \return -1 if error; index of on_process_event if success */
+int sdl_add_cb_on_process_event(SDL_PROCESS_EVENT_CB cb);
+
+/* SDL Add on_update Callback
+ \return -1 if error; index of on_update if success */
+int sdl_add_cb_on_update(SDL_UPDATE_CB cb);
 
 #endif
