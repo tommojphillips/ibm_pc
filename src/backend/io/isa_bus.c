85525312c95265613dc8257c6007d868bf01ea0c
Tommo J. Armytage <tommojphillips@gmail.com>
2025-10-27 18:54:26 +1000

    # refactored isa_bus.c # added name var to ISA_BUS

--- src/backend/io/isa_bus.c
+++ src/backend/io/isa_bus.c
@@ -6,6 +6,7 @@
 #include <stdint.h>
 #include <malloc.h>
 #include <memory.h>
+#include <string.h>
 
 #include "isa_bus.h"
 #include "memory_map.h"
@@ -18,6 +19,8 @@
 #define dbg_print(x, ...)
 #endif
 
+#define ISA_CARD_NAME_SIZE 256
+
 #define IS_REMOVED(i)  (bus->cards[i].flags & ISA_CARD_FLAG_REMOVED)
 #define IS_ENABLED(i)  (bus->cards[i].flags & ISA_CARD_FLAG_ENABLED)
 #define IS_ACTIVE(i)   (!IS_REMOVED(i) && IS_ENABLED(i))
@@ -27,7 +30,7 @@
 
 int isa_bus_create(ISA_BUS* bus, MEMORY_MAP* map, int slots) {
 	if (bus != NULL) {
-		bus->cards = (ISA_CARD*)calloc(slots, sizeof(ISA_CARD));
+		bus->cards = calloc(slots, sizeof(ISA_CARD));
 		if (bus->cards == NULL) {
 			dbg_print("Failed to create isa bus; Calloc failed. slot_count = %x, isa_card_size = %zu\n", slots, sizeof(ISA_CARD));
 			return 1;
@@ -35,23 +38,45 @@ int isa_bus_create(ISA_BUS* bus, MEMORY_MAP* map, int slots) {
 		bus->card_count = slots;
 		bus->card_index = 0;
 		bus->map = map;
+
+		for (int i = 0; i < slots; ++i) {
+			bus->cards[i].name = calloc(1, ISA_CARD_NAME_SIZE);
+			if (bus->cards[i].name == NULL) {
+				dbg_print("Failed to create isa card; Calloc failed. slots = %x, slot = %x\n", slots, i);
+				return 1;
+			}
+		}
+
 		return 0;
 	}
-	dbg_print("Failed to create isa bus; Bus was NULL.\n");
+	dbg_print("Failed to create isa bus; bus was NULL.\n");
 	return 1;
 }
 void isa_bus_destroy(ISA_BUS* bus) {
 	if (bus != NULL) {		
 		if (bus->cards != NULL) {
+			for (int i = 0; i < bus->card_count; ++i) {
+				bus->cards[i].mregion_index = -1;
+				bus->cards[i].flags = 0;
+				bus->cards[i].write_io_byte = NULL;
+				bus->cards[i].read_io_byte = NULL;
+				bus->cards[i].param = NULL;
+				if (bus->cards[i].name != NULL) {
+					free(bus->cards[i].name);
+					bus->cards[i].name = NULL;
+				}
+			}
+
 			free(bus->cards);
 			bus->cards = NULL;
 		}
 		bus->card_count = 0;
 		bus->card_index = 0;
+		bus->map = NULL;
 	}
 }
 
-int isa_bus_add_card(ISA_BUS* bus) {
+int isa_bus_add_card(ISA_BUS* bus, const char* name) {
 	int index = -1;
 
 	/* find first removed card */
@@ -76,19 +101,45 @@ int isa_bus_add_card(ISA_BUS* bus) {
 	bus->cards[index].read_io_byte = NULL;
 	bus->cards[index].param = NULL;
 	bus->cards[index].flags = ISA_CARD_FLAG_ENABLED;
+
+	if (name != NULL) {
+		strncpy_s(bus->cards[index].name, ISA_CARD_NAME_SIZE, name, ISA_CARD_NAME_SIZE - 1);
+	}
+	else {
+		sprintf(bus->cards[index].name, "Unknown Card %d", index);
+	}
 	return index;
 }
 int isa_bus_remove_card(ISA_BUS* bus, int index) {
-	if (IS_IN_RANGE(index)) {
+	if (IS_IN_RANGE(index) && !IS_REMOVED(index)) {
+
+		if (HAS_MM(index)) {
+			isa_card_remove_mm(bus, index);
+		}
+
+		if (HAS_IO(index)) {
+			isa_card_remove_io(bus, index);
+		}
+
 		bus->cards[index].mregion_index = -1;
 		bus->cards[index].write_io_byte = NULL;
 		bus->cards[index].read_io_byte = NULL;
 		bus->cards[index].param = NULL;
 		bus->cards[index].flags = ISA_CARD_FLAG_REMOVED;
+		bus->cards[index].name[0] = '\0';
 		return 0;
 	}
-	dbg_print("Failed to remove isa card; Index out of range. index = %d\n", index);
+	dbg_print("Failed to remove isa card; Index out of range or card removed. index = %d, removed = %d\n", index, IS_REMOVED(index));
 	return 1;
+}
+int isa_bus_remove_all_cards(ISA_BUS* bus) {
+	int r = 0;
+	for (int i = 0; i < bus->card_index; ++i) {
+		if (isa_bus_remove_card(bus, i)) {
+			r = 1;
+		}
+	}
+	return r;
 }
 
 int isa_bus_enable_card(ISA_BUS* bus, int index) {
@@ -100,7 +151,7 @@ int isa_bus_enable_card(ISA_BUS* bus, int index) {
 		}
 		return 0;
 	}
-	dbg_print("Failed to enable isa card; Index out of range. index = %d\n", index);
+	dbg_print("Failed to enable isa card; Index out of range or card removed. index = %d, removed = %d\n", index, IS_REMOVED(index));
 	return 1;
 }
 int isa_bus_disable_card(ISA_BUS* bus, int index) {
@@ -112,7 +163,7 @@ int isa_bus_disable_card(ISA_BUS* bus, int index) {
 		}
 		return 0;
 	}
-	dbg_print("Failed to disable isa card; Index out of range. index = %d\n", index);
+	dbg_print("Failed to disable isa card; Index out of range or card removed. index = %d, removed = %d\n", index, IS_REMOVED(index));
 	return 1;
 }
 
@@ -149,6 +200,17 @@ int isa_card_add_mm(ISA_BUS* bus, int index, uint32_t start, uint32_t size, uint
 	dbg_print("Failed to add MM to isa card; Index out of range or card removed. index = %d, removed = %d\n", index, IS_REMOVED(index));
 	return 1;
 }
+int isa_card_remove_mm(ISA_BUS* bus, int index) {
+	if (IS_IN_RANGE(index) && !IS_REMOVED(index)) {
+		bus->cards[index].flags &= ~ISA_CARD_FLAG_HAS_MM;
+		int r = memory_map_remove_mregion(bus->map, bus->cards[index].mregion_index);
+		bus->cards[index].mregion_index = -1;
+		return r;
+	}
+	dbg_print("Failed to remove MM from isa card; Index out of range or card removed. index = %d, removed = %d\n", index, IS_REMOVED(index));
+	return 1;
+}
+
 int isa_card_add_io(ISA_BUS* bus, int index, ISA_BUS_WRITE_IO write_io_byte, ISA_BUS_READ_IO read_io_byte, void* param) {
 	if (IS_IN_RANGE(index) && !IS_REMOVED(index)) {
 		bus->cards[index].flags |= ISA_CARD_FLAG_HAS_IO;
@@ -158,5 +220,16 @@ int isa_card_add_io(ISA_BUS* bus, int index, ISA_BUS_WRITE_IO write_io_byte, ISA
 		return 0;
 	}
 	dbg_print("Failed to add IO to isa card; Index out of range or card removed. index = %d, removed = %d\n", index, IS_REMOVED(index));
+	return 1;
+}
+int isa_card_remove_io(ISA_BUS* bus, int index) {
+	if (IS_IN_RANGE(index) && !IS_REMOVED(index)) {
+		bus->cards[index].flags &= ~ISA_CARD_FLAG_HAS_IO;
+		bus->cards[index].write_io_byte = NULL;
+		bus->cards[index].read_io_byte = NULL;
+		bus->cards[index].param = NULL;
+		return 0;
+	}
+	dbg_print("Failed to remove IO from isa card; Index out of range or card removed. index = %d, removed = %d\n", index, IS_REMOVED(index));
 	return 1;
 }
--- src/backend/io/isa_bus.h
+++ src/backend/io/isa_bus.h
@@ -39,25 +39,26 @@ typedef int(*ISA_BUS_READ_IO)(void* param, uint16_t port, uint8_t* ptr);
 
 /* ISA Card */
 typedef struct ISA_CARD {
-	int mregion_index; /* index to mregion. used if the isa card has memory mapped (HAS_MM) */
+	int mregion_index;              /* index to mregion. used if the isa card has memory mapped (HAS_MM) */
+	uint32_t flags;                 /* flags for the isa card. ISA_CARD_FLAG_XXX */
 	ISA_BUS_WRITE_IO write_io_byte; /* write io byte. used if the isa card has io mapped (HAS_IO) */
 	ISA_BUS_READ_IO read_io_byte;   /* read io byte. used if the isa card has io mapped (HAS_IO) */
-	void* param; /* param passed to read/write io funcs. used if the isa card has io mapped (HAS_IO) */
-	uint32_t flags; /* flags for the isa card. ISA_CARD_FLAG_XXX */
+	void* param;                    /* param passed to read/write io funcs. used if the isa card has io mapped (HAS_IO) */
+	char* name;
 } ISA_CARD;
 
 /* ISA BUS */
 typedef struct ISA_BUS {
-	ISA_CARD* cards;
 	int card_count;  /* max ISA CARDS */
 	int card_index;  /* current ISA CARDS */
+	ISA_CARD* cards;
 	MEMORY_MAP* map; /* memory map struct ptr */
 } ISA_BUS;
 
 /* Create an ISA Bus
 	bus:	the bus instance
 	map:	the memory map instance
-	slots:  the number of card slots on the bus
+	slots:  the number of isa card slots on the bus
 	Returns: 1 if error. 0 on success */
 int isa_bus_create(ISA_BUS* bus, MEMORY_MAP* map, int slots);
 
@@ -67,30 +68,36 @@ void isa_bus_destroy(ISA_BUS* bus);
 
 /* Add an ISA Card to the bus and enables it
    bus:     the isa bus instance
+   name:    the isa card name; can be NULL
    Returns: -1 if error or the index of the added card on success */
-int isa_bus_add_card(ISA_BUS* bus);
+int isa_bus_add_card(ISA_BUS* bus, const char* name);
 
 /* Remove an ISA Card from the bus
    bus:     the isa bus instance
-   index:   the card index
+   index:   the isa card index
    Returns: 1 if error or 0 on success */
 int isa_bus_remove_card(ISA_BUS* bus, int index);
 
+/* Remove all ISA Cards from the bus
+   bus:     the isa bus instance
+   Returns: 1 if error or 0 on success */
+int isa_bus_remove_all_cards(ISA_BUS* bus);
+
 /* Enable an ISA Card on bus
    bus:     the isa bus instance
-   index:   the card index
+   index:   the isa card index
    Returns: 1 if error or 0 on success */
 int isa_bus_enable_card(ISA_BUS* bus, int index);
 
 /* Disable an ISA Card from the bus
    bus:     the isa bus instance
-   index:   the card index
+   index:   the isa card index
    Returns: 1 if error or 0 on success */
 int isa_bus_disable_card(ISA_BUS* bus, int index);
 
 /* Add a memory region to an ISA CARD
    bus:     the isa bus instance
-   index:   the card index
+   index:   the isa card index
    start:   the region start
    size:    the region size
    mask:    the region address mask
@@ -98,14 +105,26 @@ int isa_bus_disable_card(ISA_BUS* bus, int index);
    Returns: 1 if error or 0 on success */
 int isa_card_add_mm(ISA_BUS* bus, int index, uint32_t start, uint32_t size, uint32_t mask, uint32_t flags);
 
+/* Remove mregon from an ISA Card on the bus
+   bus:     the isa bus instance
+   index:   the isa card index
+   Returns: 1 if error or 0 on success */
+int isa_card_remove_mm(ISA_BUS* bus, int index);
+
 /* Add port mapped io to an ISA CARD
    bus:           the isa bus instance
-   index:         the card index
+   index:         the isa card index
    write_io_byte: write io byte function
    read_io_byte:  read io byte function
    param:         the param to pass to the write and read functions.
    Returns:       1 if error or 0 on success */
 int isa_card_add_io(ISA_BUS* bus, int index, ISA_BUS_WRITE_IO write_io_byte, ISA_BUS_READ_IO read_io_byte, void* param);
+
+/* Remove io from an ISA Card on the bus
+   bus:     the isa bus instance
+   index:   the isa card index
+   Returns: 1 if error or 0 on success */
+int isa_card_remove_io(ISA_BUS* bus, int index);
 
 /* ISA Bus Read port mapped io
 	port:   the IO port address
