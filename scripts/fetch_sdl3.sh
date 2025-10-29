e622f3fadb491a50c01c594f3f75a8a4a82dcbed
Tommo J. Armytage <tommojphillips@gmail.com>
2025-10-29 13:53:37 +1000

    # Updated fetch_sdl3.sh script.

--- scripts/fetch_sdl3.sh
+++ scripts/fetch_sdl3.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 set -e
 
-# --- Config: Releases and download URLs ---
+# Download URLs
 SDL3_VER=3.2.24
 SDL3_TTF_VER=3.2.2
 declare -A RELEASES
-RELEASES["SDL3_v$SDL3_VER"]="https://github.com/libsdl-org/SDL/releases/download/release-3.2.24/SDL3-devel-$SDL3_VER-VC.zip"
-RELEASES["SDL3_ttf_v$SDL3_TTF_VER"]="https://github.com/libsdl-org/SDL_ttf/releases/download/release-3.2.2/SDL3_ttf-devel-3.2.2-VC.zip"
+RELEASES["SDL3 v$SDL3_VER"]="https://github.com/libsdl-org/SDL/releases/download/release-$SDL3_VER/SDL3-devel-$SDL3_VER-VC.zip"
+RELEASES["SDL3_ttf v$SDL3_TTF_VER"]="https://github.com/libsdl-org/SDL_ttf/releases/download/release-$SDL3_TTF_VER/SDL3_ttf-devel-$SDL3_TTF_VER-VC.zip"
 
-# --- Config: Destinations (space-separated strings) ---
+# Destinations (space separated strings)
 declare -A DESTS
-DESTS["SDL3_v$SDL3_VER"]="lib/SDL3 lib/UI/lib/SDL3"
-DESTS["SDL3_ttf_v$SDL3_TTF_VER"]="lib/SDL3_ttf"
+DESTS["SDL3 v$SDL3_VER"]="lib/SDL3 lib/UI/lib/SDL3"
+DESTS["SDL3_ttf v$SDL3_TTF_VER"]="lib/SDL3_ttf"
 
 # Temporary files/folders
 TMP_ZIP="tmp.zip"
 TMP_DIR="tmp_extract"
-BASE_DIR="external"
-
-mkdir -p "$BASE_DIR"
 
 for NAME in "${!RELEASES[@]}"; do
     URL="${RELEASES[$NAME]}"
@@ -28,7 +25,7 @@ for NAME in "${!RELEASES[@]}"; do
     mkdir -p "$TMP_DIR"
     unzip -oq "$TMP_ZIP" -d "$TMP_DIR"
 
-    # Convert space-separated string to array
+    # Convert space separated string to array
     read -r -a DEST_ARRAY <<< "${DESTS[$NAME]}"
     
     SRC_DIR=$(find "$TMP_DIR" -mindepth 1 -maxdepth 1 -type d)
