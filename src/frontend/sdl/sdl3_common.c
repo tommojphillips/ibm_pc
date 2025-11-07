fef10790686b1271c702308c84a372327aa1a666
Tommo J. Armytage <tommojphillips@gmail.com>
2025-11-07 18:24:40 +1000

    # Added on_update cb user params to SDL # Added on_process_event cb user params to SDL. # Removed global SDL var. SDL var is now passed in to functions.

--- src/frontend/sdl/sdl3_common.c
+++ src/frontend/sdl/sdl3_common.c
@@ -17,24 +17,16 @@
 #define dbg_print(x, ...)
 #endif
 
-/* Global SDL State */
-SDL* sdl = NULL;
-
 /* SDL struct */
-int sdl_create(void) {
-	if (sdl != NULL) {
-		dbg_print("Error: Global SDL instance already created\n");
+int sdl_create(SDL** sdl) {
+	if (*sdl != NULL) {
+		dbg_print("Error: SDL instance already created\n");
 		return 1;
 	}
 
-	sdl = calloc(1, sizeof(SDL));
-	if (sdl == NULL) {
-		dbg_print("Failed to allocate Global SDL instance\n");
-		return 1;
-	}
-
-	if (ring_buffer_create(&sdl->key_state, KEYS_SIZE)) {
-		dbg_print("Failed to allocate key state ring buffer\n");
+	*sdl = calloc(1, sizeof(SDL));
+	if (*sdl == NULL) {
+		dbg_print("Failed to allocate SDL instance\n");
 		return 1;
 	}
 
@@ -50,33 +42,36 @@ int sdl_create(void) {
 		return 1;
 	}
 
-	sdl->quit = 0;
+	(*sdl)->quit = 0;
 
-	sdl->on_process_event = NULL;
-	sdl->on_process_event_count = 0;
-	sdl->on_process_event_index = 0;
+	(*sdl)->on_process_event = NULL;
+	(*sdl)->on_process_event_count = 0;
+	(*sdl)->on_process_event_index = 0;
 	
-	sdl->on_update = NULL;
-	sdl->on_update_count = 0;
-	sdl->on_update_index = 0;
+	(*sdl)->on_update = NULL;
+	(*sdl)->on_update_count = 0;
+	(*sdl)->on_update_index = 0;
 	return 0;
 }
-void sdl_destroy(void) {
+void sdl_destroy(SDL* sdl) {
 
 	if (sdl != NULL) {
 
 		TTF_Quit();
 		SDL_QuitSubSystem(SDL_INIT_VIDEO);
 		SDL_Quit();
-
-		ring_buffer_destroy(&sdl->key_state);
-
+				
 		if (sdl->on_update != NULL) {
 			free(sdl->on_update);
 			sdl->on_update = NULL;
 		}
 		sdl->on_update_count = 0;
 		sdl->on_update_index = 0;
+
+		if (sdl->on_update_param1 != NULL) {
+			free(sdl->on_update_param1);
+			sdl->on_update_param1 = NULL;
+		}
 
 		if (sdl->on_process_event != NULL) {
 			free(sdl->on_process_event);
@@ -85,17 +80,22 @@ void sdl_destroy(void) {
 		sdl->on_process_event_count = 0;
 		sdl->on_process_event_index = 0;
 
+		if (sdl->on_process_event_param1 != NULL) {
+			free(sdl->on_process_event_param1);
+			sdl->on_process_event_param1 = NULL;
+		}
+
 		/* Free Global SDL State */
 		free(sdl);
 		sdl = NULL;
 	}
 }
 
-void sdl_update(void) {
+void sdl_update(SDL* sdl) {
 	while (SDL_PollEvent(&sdl->e)) {
 		/* call on_process_event() */
 		for (int i = 0; i < sdl->on_process_event_index; ++i) {
-			if (sdl->on_process_event[i](&sdl->e)) {
+			if (sdl->on_process_event[i](sdl->on_process_event_param1[i], &sdl->e)) {
 				sdl->quit = 1;
 				return;
 			}
@@ -104,43 +104,69 @@ void sdl_update(void) {
 
 	/* call on_update() */
 	for (int i = 0; i < sdl->on_update_index; ++i) {
-		sdl->on_update[i]();
+		sdl->on_update[i](sdl->on_update_param1[i]);
 	}
 }
 
-int sdl_add_cb_on_process_event(SDL_PROCESS_EVENT_CB cb) {
+int sdl_add_cb_on_process_event(SDL* sdl, SDL_PROCESS_EVENT_CB cb, void* cb_param1) {
+	if (sdl == NULL) {
+		printf("[SDL] Failed to add on_process_event; SDL was NULL\n");
+		return -1;
+	}
+
 	int index = sdl->on_process_event_index;
 
 	if (index >= sdl->on_process_event_count) {
 		SDL_PROCESS_EVENT_CB* new_cb = realloc(sdl->on_process_event, ((size_t)sdl->on_process_event_count + 1) * sizeof(SDL_PROCESS_EVENT_CB));
 		if (new_cb == NULL) {
-			printf("[SDL] Failed to realloc on_process_event\n");
+			printf("[SDL] Failed to add on_process_event; Realloc failed\n");
 			return -1;
 		}
 
+		void* new_param1 = realloc(sdl->on_process_event_param1, (sdl->on_process_event_count + 1) * sizeof(void*));
+		if (new_param1 == NULL) {
+			dbg_print("[SDL]  Failed to add on_process_event; Realloc failed\n");
+			return -1;
+		}
+		
 		sdl->on_process_event = new_cb;
-		sdl->on_process_event_count += 1;
+		sdl->on_process_event_param1 = new_param1;
+		sdl->on_process_event_count++;
 	}
 
 	sdl->on_process_event[index] = cb;
+	sdl->on_process_event_param1[index] = cb_param1;
 	sdl->on_process_event_index++;
 	return index;
 }
-int sdl_add_cb_on_update(SDL_UPDATE_CB cb) {
-	int index = sdl->on_update_index;
+int sdl_add_cb_on_update(SDL* sdl, SDL_UPDATE_CB cb, void* cb_param1) {
+	if (sdl == NULL) {
+		printf("[SDL] Failed to add on_update; SDL was NULL\n");
+		return -1;
+	}
 
+	int index = sdl->on_update_index;
+	
 	if (index >= sdl->on_update_count) {
-		SDL_UPDATE_CB* new_cb = realloc(sdl->on_update, ((size_t)sdl->on_update_count + 1) * sizeof(SDL_UPDATE_CB));
+		SDL_UPDATE_CB* new_cb = realloc(sdl->on_update, (sdl->on_update_count + 1) * sizeof(SDL_UPDATE_CB));
 		if (new_cb == NULL) {
-			printf("[SDL] Failed to realloc on_update\n");
+			dbg_print("[SDL] Failed to add on_update; Realloc failed\n");
+			return -1;
+		}
+
+		void* new_param1 = realloc(sdl->on_update_param1, (sdl->on_update_count + 1) * sizeof(void*));
+		if (new_param1 == NULL) {
+			dbg_print("[SDL] Failed to add on_update; Realloc failed\n");
 			return -1;
 		}
 
 		sdl->on_update = new_cb;
-		sdl->on_update_count += 1;
+		sdl->on_update_param1 = new_param1;
+		sdl->on_update_count++;
 	}
 
 	sdl->on_update[index] = cb;
+	sdl->on_update_param1[index] = cb_param1;
 	sdl->on_update_index++;
 	return index;
 }
--- src/frontend/sdl/sdl3_common.h
+++ src/frontend/sdl/sdl3_common.h
@@ -9,48 +9,41 @@
 
 #include <SDL3/SDL.h>
 
-#include "backend/ring_buffer.h"
-
-#define KEYS_SIZE 10
-
-typedef int(*SDL_PROCESS_EVENT_CB)(SDL_Event* e);
-typedef void(*SDL_UPDATE_CB)(void);
+typedef int(*SDL_PROCESS_EVENT_CB)(void* param1, SDL_Event* e);
+typedef void(*SDL_UPDATE_CB)(void* param1);
 
 /* Window SDL */
 typedef struct SDL {
 	SDL_Event e;
 	int quit;
-	RING_BUFFER key_state;
-	int kbd_new_input;
 
 	SDL_UPDATE_CB* on_update;
+	void** on_update_param1;
 	int32_t on_update_count;
 	int32_t on_update_index;
 
 	SDL_PROCESS_EVENT_CB* on_process_event;
+	void** on_process_event_param1;
 	int32_t on_process_event_count;
 	int32_t on_process_event_index;
 } SDL;
 
-/* Global SDL State */
-extern SDL* sdl;
-
 /* SDL Create 
  \return 1 if error; 0 if success */
-int sdl_create(void);
+int sdl_create(SDL** sdl);
 
 /* SDL Destroy */
-void sdl_destroy(void);
+void sdl_destroy(SDL* sdl);
 
 /* SDL Update */
-void sdl_update(void);
+void sdl_update(SDL* sdl);
 
 /* SDL Add on_process_event Callback 
  \return -1 if error; index of on_process_event if success */
-int sdl_add_cb_on_process_event(SDL_PROCESS_EVENT_CB cb);
+int sdl_add_cb_on_process_event(SDL* sdl, SDL_PROCESS_EVENT_CB cb, void* cb_param1);
 
 /* SDL Add on_update Callback
  \return -1 if error; index of on_update if success */
-int sdl_add_cb_on_update(SDL_UPDATE_CB cb);
+int sdl_add_cb_on_update(SDL* sdl, SDL_UPDATE_CB cb, void* cb_param1);
 
 #endif
