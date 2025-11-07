81a04477fbb056991abbe79c05a05f75d6b5cf60
Tommo J. Armytage <tommojphillips@gmail.com>
2025-11-07 18:21:07 +1000

    # Refactored sdl3_window.c # Removed global window_manager var. a window_manger instance is now passed in to the functions. # Added a 2nd user param to the window instance on_render cb

--- src/frontend/sdl/sdl3_window.c
+++ src/frontend/sdl/sdl3_window.c
@@ -22,12 +22,9 @@
 #define dbg_print(x, ...)
 #endif
 
-/* Global window manager state */
-WINDOW_MANAGER* window_manager = NULL;
-
 /* Window Instance */
-int window_instance_create(WINDOW_INSTANCE** instance) {
-	if (window_manager == NULL) {
+int window_instance_create(WINDOW_MANAGER* manager, WINDOW_INSTANCE** instance) {
+	if (manager == NULL) {
 		dbg_print("Failed to create window instance. Window manager is NULL\n");
 		return 1;
 	}
@@ -35,36 +32,37 @@ int window_instance_create(WINDOW_INSTANCE** instance) {
 	int index = -1;
 
 	/* find first removed instance */
-	for (int i = 0; i < window_manager->instance_index; ++i) {
-		if (!(window_manager->instances[i].window_state & WINDOW_INSTANCE_STATE_CREATED)) {
+	for (int i = 0; i < manager->instance_index; ++i) {
+		if (!(manager->instances[i].window_state & WINDOW_INSTANCE_STATE_CREATED)) {
 			index = i;
 		}
 	}
 
 	/* did not find a removed instance; add to end */
 	if (index < 0) {
-		index = window_manager->instance_index;
-		if (index >= window_manager->instance_count) {
+		index = manager->instance_index;
+		if (index >= manager->instance_count) {
 			dbg_print("Failed to create window instance. Window instance index out of range.\n");
 			return 1;
 		}
-		window_manager->instance_index++;
+		manager->instance_index++;
 	}
 
-	memset(&window_manager->instances[index], 0, sizeof(WINDOW_INSTANCE));
-	window_manager->instances[index].window_state = WINDOW_INSTANCE_STATE_CREATED;
-	*instance = &window_manager->instances[index];
+	*instance = &manager->instances[index];
+	memset(*instance, 0, sizeof(WINDOW_INSTANCE));
+	(*instance)->manager = manager;
+	(*instance)->window_state = WINDOW_INSTANCE_STATE_CREATED;
 	return 0;
 }
 int window_instance_destroy(WINDOW_INSTANCE* instance) {
 
-	if (window_manager == NULL) {
-		dbg_print("Failed to destroy window instance. Window manager is NULL\n");
+	if (instance == NULL) {
+		dbg_print("Failed to destroy window instance. Window instance is NULL\n");
 		return 1;
 	}
 
-	if (instance == NULL) {
-		dbg_print("Failed to destroy window instance. Window instance is NULL\n");
+	if (instance->manager == NULL) {
+		dbg_print("Failed to destroy window instance. Window manager is NULL\n");
 		return 1;
 	}
 
@@ -86,9 +84,14 @@ int window_instance_destroy(WINDOW_INSTANCE* instance) {
 	instance->on_render_count = 0;
 	instance->on_render_index = 0;
 
-	if (instance->on_render_param != NULL) {
-		free(instance->on_render_param);
-		instance->on_render_param = NULL;
+	if (instance->on_render_param1 != NULL) {
+		free(instance->on_render_param1);
+		instance->on_render_param1 = NULL;
+	}
+
+	if (instance->on_render_param2 != NULL) {
+		free(instance->on_render_param2);
+		instance->on_render_param2 = NULL;
 	}
 
 	instance->window_state &= ~WINDOW_INSTANCE_STATE_CREATED;		
@@ -96,65 +99,65 @@ int window_instance_destroy(WINDOW_INSTANCE* instance) {
 }
 
 int window_instance_open(WINDOW_INSTANCE* instance) {
-	
-	if (window_manager == NULL) {
-		dbg_print("Failed to open window instance. Window manager is NULL\n");
-		return 1;
-	}
 
 	if (instance == NULL) {
-		dbg_print("Failed to open window instance. Window instance is NULL\n");
+		dbg_print("[WINDOW] Failed to open window instance. Window instance is NULL\n");
+		return 1;
+	}
+	
+	if (instance->manager == NULL) {
+		dbg_print("[WINDOW] Failed to open window instance. Window manager is NULL\n");
 		return 1;
 	}
 
 	if ((instance->window_state & WINDOW_INSTANCE_STATE_CREATED) == 0) {
-		dbg_print("Failed to open window instance. Window instance not created\n");
+		dbg_print("[WINDOW] Failed to open window instance. Window instance not created\n");
 		return 1;
 	}
 
 	if (instance->window_state & WINDOW_INSTANCE_STATE_OPEN) {
-		dbg_print("Failed to open window instance. Window instance already open\n");
+		dbg_print("[WINDOW] Failed to open window instance. Window instance already open\n");
 		return 1;
 	}
 
 	/* init window */
 	instance->window = SDL_CreateWindow(instance->title, instance->transform.w, instance->transform.h, SDL_WINDOW_RESIZABLE);
 	if (instance->window == NULL) {
-		dbg_print("Failed to open window instance. Could not create window: %s\n", SDL_GetError());
+		dbg_print("[WINDOW] Failed to open window instance. Could not create window: %s\n", SDL_GetError());
 		return 1;
 	}
 
 	/* create renderer */
 	instance->renderer = SDL_CreateRenderer(instance->window, NULL);
 	if (instance->renderer == NULL) {
-		dbg_print("Failed to open window instance. Could not create renderer: %s\n", SDL_GetError());
+		dbg_print("[WINDOW] Failed to open window instance. Could not create renderer: %s\n", SDL_GetError());
 		return 1;
 	}
 
 	/* get window id */
 	instance->window_id = SDL_GetWindowID(instance->window);
 	if (instance->window_id == 0) {
-		dbg_print("Failed to open window instance. Could not get window id: %s\n", SDL_GetError());
+		dbg_print("[WINDOW] Failed to open window instance. Could not get window id: %s\n", SDL_GetError());
 		return 1;
 	}
 
 	SDL_SetWindowPosition(instance->window, instance->transform.x, instance->transform.y);
 
-	instance->text_engine = window_manager->text_engine;
+	instance->text_engine = instance->manager->text_engine;
 
 	instance->window_state |= WINDOW_INSTANCE_STATE_OPEN;
-	window_manager->instances_open++;
+	instance->manager->instances_open++;
 	return 0;
 }
 int window_instance_close(WINDOW_INSTANCE* instance) {
 
-	if (window_manager == NULL) {
-		dbg_print("Failed to close window instance. Window manager is NULL\n");
+	if (instance == NULL) {
+		dbg_print("[WINDOW] Failed to close window instance. Window instance is NULL\n");
 		return 1;
 	}
 
-	if (instance == NULL) {
-		dbg_print("Failed to close window instance. Window instance is NULL\n");
+	if (instance->manager == NULL) {
+		dbg_print("[WINDOW] Failed to close window instance. Window manager is NULL\n");
 		return 1;
 	}
 
@@ -176,7 +179,7 @@ int window_instance_close(WINDOW_INSTANCE* instance) {
 	instance->text_engine = NULL;
 
 	instance->window_state &= ~WINDOW_INSTANCE_STATE_OPEN;
-	window_manager->instances_open--;
+	instance->manager->instances_open--;
 	return 0;
 }
 
@@ -215,7 +218,11 @@ static void window_instance_render(WINDOW_INSTANCE* instance) {
 
 		// call on_render() for window instance
 		for (int i = 0; i < instance->on_process_event_index; ++i) {
-			instance->on_render[i](instance, instance->on_render_param[i]);
+			void* param1 = instance->on_render_param1[i];
+			if (param1 == NULL) {
+				param1 = instance;
+			}
+			instance->on_render[i](param1, instance->on_render_param2[i]);
 		}
 
 		SDL_RenderPresent(instance->renderer);
@@ -266,134 +273,176 @@ void window_instance_toggle_full_screen(WINDOW_INSTANCE* instance) {
 	SDL_SetWindowFullscreen(instance->window, fullscreen);
 }
 
-int window_instance_set_cb_on_process_event(WINDOW_INSTANCE* instance, WINDOW_INSTANCE_CB cb) {
-	if (instance != NULL) {
+int window_instance_add_cb_on_process_event(WINDOW_INSTANCE* instance, WINDOW_INSTANCE_CB_ON_PROCESS_EVENT cb) {
+	if (instance == NULL) {
+		dbg_print("[WINDOW] Failed to add cb on_process_event. Window instance is NULL\n");
+		return -1;
+	}
 
-		int index = instance->on_process_event_index;
+	int index = instance->on_process_event_index;
 
-		if (index >= instance->on_process_event_count) {
-			WINDOW_INSTANCE_CB* new_cb = realloc(instance->on_process_event, (instance->on_process_event_count + 1) * sizeof(WINDOW_INSTANCE_CB));
-			if (new_cb == NULL) {
-				printf("[WINDOW] Failed to realloc process_event\n");
-				return -1;
-			}
-
-			instance->on_process_event = new_cb;
-			instance->on_process_event_count += 1;
+	if (index >= instance->on_process_event_count) {
+		WINDOW_INSTANCE_CB_ON_PROCESS_EVENT* new_cb = realloc(instance->on_process_event, (instance->on_process_event_count + 1) * sizeof(WINDOW_INSTANCE_CB_ON_PROCESS_EVENT));
+		if (new_cb == NULL) {
+			printf("[WINDOW] Failed to add cb on_process_event. Realloc failed for on_process_event\n");
+			return -1;
 		}
 
-		instance->on_process_event[index] = cb;
-		instance->on_process_event_index++;
-		return index;
+		instance->on_process_event = new_cb;
+		instance->on_process_event_count += 1;
 	}
-	return -1;
+
+	instance->on_process_event[index] = cb;
+	instance->on_process_event_index++;
+	return index;
 }
-int window_instance_set_cb_on_render(WINDOW_INSTANCE* instance, WINDOW_INSTANCE_CB cb, void* cb_param) {
-	if (instance != NULL) {
+int window_instance_add_cb_on_render(WINDOW_INSTANCE* instance, WINDOW_INSTANCE_CB cb, void* cb_param1, void* cb_param2) {
+	if (instance == NULL) {
+		dbg_print("[WINDOW] Failed to add cb on_render. Window instance is NULL\n");
+		return -1;
+	}
 
-		int index = instance->on_render_index;
+	int index = instance->on_render_index;
 
-		if (index >= instance->on_render_count) {
-			WINDOW_INSTANCE_CB* new_cb = realloc(instance->on_render, (instance->on_render_count + 1) * sizeof(WINDOW_INSTANCE_CB));
-			if (new_cb == NULL) {
-				dbg_print("[WINDOW] Failed to realloc on_render\n");
-				return -1;
-			}
+	if (index >= instance->on_render_count) {
+		WINDOW_INSTANCE_CB* new_cb = realloc(instance->on_render, (instance->on_render_count + 1) * sizeof(WINDOW_INSTANCE_CB));
+		if (new_cb == NULL) {
+			dbg_print("[WINDOW] Failed to add cb on_render. Realloc failed for on_render\n");
+			return -1;
+		}
 			
-			void* new_param = realloc(instance->on_render_param, (instance->on_render_count + 1) * sizeof(void*));
-			if (new_param == NULL) {
-				dbg_print("[WINDOW] Failed to realloc on_render_param\n");
-				return -1;
-			}
-
-			instance->on_render = new_cb;
-			instance->on_render_param = new_param;
-			instance->on_render_count += 1;
+		void* new_param1 = realloc(instance->on_render_param1, (instance->on_render_count + 1) * sizeof(void*));
+		if (new_param1 == NULL) {
+			dbg_print("[WINDOW] Failed to add cb on_render. Realloc failed for on_render_param1\n");
+			return -1;
+		}
+			
+		void* new_param2 = realloc(instance->on_render_param2, (instance->on_render_count + 1) * sizeof(void*));
+		if (new_param2 == NULL) {
+			dbg_print("[WINDOW] Failed to add cb on_render. Realloc failed for on_render_param2\n");
+			return -1;
 		}
 
-		instance->on_render[index] = cb;
-		instance->on_render_param[index] = cb_param;
-		instance->on_render_index++;
-		return index;
+		instance->on_render = new_cb;
+		instance->on_render_param1 = new_param1;
+		instance->on_render_param2 = new_param2;
+		instance->on_render_count += 1;
 	}
-	return -1;
+
+	instance->on_render[index] = cb;
+	instance->on_render_param1[index] = cb_param1;
+	instance->on_render_param2[index] = cb_param2;
+	instance->on_render_index++;
+	return index;
 }
 
-/* Window Instance Manager */
-int window_manager_create(uint16_t window_count) {
-	if (window_manager != NULL) {
-		dbg_print("Error: Window Manager already created\n");
+int window_instance_set_cb_on_process_event(WINDOW_INSTANCE* instance, int index, WINDOW_INSTANCE_CB_ON_PROCESS_EVENT cb) {
+	if (instance == NULL) {
+		dbg_print("[WINDOW] Failed to set cb on_process_event. Window instance is NULL\n");
+		return 1;
+	}
+	
+	if (index >= instance->on_process_event_index) {
+		dbg_print("[WINDOW] Failed to set cb on_process_event. Index out of range\n");
 		return 1;
 	}
 
-	window_manager = calloc(1, sizeof(WINDOW_MANAGER));
-	if (window_manager == NULL) {
-		dbg_print("Error: Failed to allocate Window Manager\n");
+	instance->on_process_event[index] = cb;
+	return 0;
+}
+int window_instance_set_cb_on_render(WINDOW_INSTANCE* instance, int index, WINDOW_INSTANCE_CB cb, void* cb_param1, void* cb_param2) {
+	if (instance == NULL) {
+		dbg_print("[WINDOW] Failed to set cb on_render. Window instance is NULL\n");
+		return 1;
+	}
+
+	if (index >= instance->on_render_index) {
+		dbg_print("[WINDOW] Failed to set cb on_render. Index out of range\n");
+		return 1;
+	}
+
+	instance->on_render[index] = cb;
+	instance->on_render_param1[index] = cb_param1;
+	instance->on_render_param2[index] = cb_param2;
+	return 0;
+}
+
+/* Window Instance Manager */
+int window_manager_create(WINDOW_MANAGER** manager, uint16_t window_count) {
+	if (*manager != NULL) {
+		dbg_print("[WINDOW MANAGER] Failed to create window manager. manager not NULL\n");
+		return 1;
+	}
+
+	*manager = calloc(1, sizeof(WINDOW_MANAGER));
+	if (*manager == NULL) {
+		dbg_print("[WINDOW MANAGER] Failed to create window manager. Could not allocate memory for window manager\n");
 		return 1;
 	}
 
 	/* init text engine */
-	window_manager->text_engine = TTF_CreateSurfaceTextEngine();
-	if (window_manager->text_engine == NULL) {
-		dbg_print("Failed to init text engine: %s\n", SDL_GetError());
+	(*manager)->text_engine = TTF_CreateSurfaceTextEngine();
+	if ((*manager)->text_engine == NULL) {
+		dbg_print("[WINDOW MANAGER] Failed to create window manager. Could not init text engine: %s\n", SDL_GetError());
 		return 1;
 	}
 
-	window_manager->instances = calloc(window_count, sizeof(WINDOW_INSTANCE));
-	if (window_manager->instances == NULL) {
-		dbg_print("Failed to allocate window instances\n");
+	(*manager)->instances = calloc(window_count, sizeof(WINDOW_INSTANCE));
+	if ((*manager)->instances == NULL) {
+		dbg_print("[WINDOW MANAGER] Failed to create window manager. Could not allocate memory for window instances\n");
 		return 1;
 	}
-	window_manager->instance_count = window_count;
-	window_manager->instance_index = 0;
+	(*manager)->instance_count = window_count;
+	(*manager)->instance_index = 0;
 	return 0;
 }
-void window_manager_destroy(void) {
-	if (window_manager != NULL) {
-		if (window_manager->instances != NULL) {
+void window_manager_destroy(WINDOW_MANAGER* manager) {
+	if (manager != NULL) {
+		if (manager->instances != NULL) {
 
-			for (int i = 0; i < window_manager->instance_index; ++i) {
-				window_instance_close(&window_manager->instances[i]);
-				window_instance_destroy(&window_manager->instances[i]);
+			for (int i = 0; i < manager->instance_index; ++i) {
+				window_instance_close(&manager->instances[i]);
+				window_instance_destroy(&manager->instances[i]);
 			}
 
-			free(window_manager->instances);
-			window_manager->instances = NULL;
+			free(manager->instances);
+			manager->instances = NULL;
 		}
-		window_manager->instance_count = 0;
-		window_manager->instance_index = 0;
+		manager->instance_count = 0;
+		manager->instance_index = 0;
 
 		/* Free text engine */
-		if (window_manager->text_engine != NULL) {
-			TTF_DestroySurfaceTextEngine(window_manager->text_engine);
-			window_manager->text_engine = NULL;
+		if (manager->text_engine != NULL) {
+			TTF_DestroySurfaceTextEngine(manager->text_engine);
+			manager->text_engine = NULL;
 		}
 
-		/* Free Global window manager instance */
-		free(window_manager);
-		window_manager = NULL;
+		/* Free window manager instance */
+		free(manager);
+		manager = NULL;
 	}
 }
-int window_manager_process_event(SDL_Event* e) {
-	for (int i = 0; i < window_manager->instance_index; ++i) {
-		if ((window_manager->instances[i].window_state & WINDOW_INSTANCE_STATE_OPEN) && e->window.windowID == window_manager->instances[i].window_id) {
-			window_instance_process_event(&window_manager->instances[i], e);
+
+int window_manager_process_event(WINDOW_MANAGER* manager, SDL_Event* e) {
+	for (int i = 0; i < manager->instance_index; ++i) {
+		if ((manager->instances[i].window_state & WINDOW_INSTANCE_STATE_OPEN) && e->window.windowID == manager->instances[i].window_id) {
+			window_instance_process_event(&manager->instances[i], e);
 		}
 	}
 
 	/* If multiple windows are open and all the windows are closed at the same time, SDL_EVENT_QUIT is 
 	 never sent. Instead, a SDL_EVENT_WINDOW_DESTROYED is sent. If there is only 1 window open and its
 	 closed, then SDL_EVENT_QUIT is sent and SDL_EVENT_WINDOW_DESTROYED is not */
-	if (e->type == SDL_EVENT_WINDOW_DESTROYED && window_manager->instances_open == 0) {
+	if (e->type == SDL_EVENT_WINDOW_DESTROYED && manager->instances_open == 0) {
 		return 1; /* QUIT */
 	}
 
 	return 0;
 }
-void window_manager_update(void) {
-	for (int i = 0; i < window_manager->instance_index; ++i) {
-		if (window_manager->instances[i].window_state & WINDOW_INSTANCE_STATE_OPEN) {
-			window_instance_render(&window_manager->instances[i]);
+void window_manager_update(WINDOW_MANAGER* manager) {
+	for (int i = 0; i < manager->instance_index; ++i) {
+		if (manager->instances[i].window_state & WINDOW_INSTANCE_STATE_OPEN) {
+			window_instance_render(&manager->instances[i]);
 		}
 	}
 }
--- src/frontend/sdl/sdl3_window.h
+++ src/frontend/sdl/sdl3_window.h
@@ -9,19 +9,11 @@
 
 #include "sdl3_timing.h"
 
-#include "backend/ring_buffer.h"
-
 typedef struct SDL_Window SDL_Window;
 typedef struct SDL_Renderer SDL_Renderer;
 typedef union SDL_Event SDL_Event;
 typedef struct TTF_TextEngine TTF_TextEngine;
 typedef struct TTF_Font TTF_Font;
-
-/* actual window width */
-#define WINDOW_W (instance->transform.w)
-
-/* actual window height */
-#define WINDOW_H (instance->transform.h)
 
 #define WINDOW_INSTANCE_STATE_DESTROYED   0x0
 #define WINDOW_INSTANCE_STATE_FULL_SCREEN 0x1
@@ -48,7 +40,9 @@ typedef struct WINDOW_MANAGER {
 	uint16_t instances_open;
 } WINDOW_MANAGER;
 
-typedef void(*WINDOW_INSTANCE_CB)(void* instance, void* param);
+typedef void(*WINDOW_INSTANCE_CB_ON_PROCESS_EVENT)(WINDOW_INSTANCE* instance, SDL_Event* e);
+
+typedef void(*WINDOW_INSTANCE_CB)(void* param1, void* param2);
 
 /* Window Instance */
 typedef struct WINDOW_INSTANCE {
@@ -62,64 +56,117 @@ typedef struct WINDOW_INSTANCE {
 	const char* title;
 	
 	WINDOW_INSTANCE_CB* on_render;
-	void** on_render_param;
+	void** on_render_param1;
+	void** on_render_param2;
 	int32_t on_render_count;
 	int32_t on_render_index;
 
-	WINDOW_INSTANCE_CB* on_process_event;
+	WINDOW_INSTANCE_CB_ON_PROCESS_EVENT* on_process_event;
 	int32_t on_process_event_count;
 	int32_t on_process_event_index;
 	
 	FRAME_STATE time;
 	
+	WINDOW_MANAGER* manager;
 } WINDOW_INSTANCE;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* Create New Window Instance.
- Allocates memory for the window instance. */
-int window_instance_create(WINDOW_INSTANCE** instance);
+/* Create New Window Instance. Allocates memory for the window instance. 
+ manager:  The window manager
+ instance: The window instance 
+ Returns:  -1 if error, The index of the window if success */
+int window_instance_create(WINDOW_MANAGER* manager, WINDOW_INSTANCE** instance);
 
-/* Open Window Instance.
- Creates the window and renderer. */
+/* Open Window Instance. Creates the window and renderer. 
+ instance: The window instance 
+ Returns:  1 if error, 0 if success */
 int window_instance_open(WINDOW_INSTANCE* instance);
 
-/* Close Window Instance.
- Destroys the window and renderer. */
+/* Close Window Instance. Destroys the window and renderer. 
+ instance: The window instance 
+ Returns:  1 if error, 0 if success */
 int window_instance_close(WINDOW_INSTANCE* instance);
 
 /* Destroy Window Instance.
- Frees memory for the window instance. */
+ Frees memory for the window instance. 
+ instance: The window instance 
+ Returns:  1 if error, 0 if success */
 int window_instance_destroy(WINDOW_INSTANCE* instance);
 
-/* Set Window Instance Transform */
+/* Set Window Instance Transform 
+ instance: The window instance */
 void window_instance_set_transform(WINDOW_INSTANCE* instance, int32_t x, int32_t y, int32_t w, int32_t h);
 
-/* Set Window Instance Min Size. Window must be open. */
+/* Set Window Instance Min Size. Window must be open. 
+ instance: The window instance */
 void window_instance_set_min_size(WINDOW_INSTANCE* instance, int32_t w, int32_t h);
 
+/* Get Window Instance fullscreen. Window must be open.
+ instance: The window instance 
+ Returns:  1 if window is fullscreen, 0 if not */
 int window_instance_is_full_screen(WINDOW_INSTANCE* instance);
+
+/* Set Window Instance fullscreen. Window must be open. 
+ instance: The window instance */
 void window_instance_set_full_screen(WINDOW_INSTANCE* instance, int fullscreen);
+
+/* Toggle Window Instance fullscreen. Window must be open.
+ instance: The window instance */
 void window_instance_toggle_full_screen(WINDOW_INSTANCE* instance);
 
-int window_instance_set_cb_on_process_event(WINDOW_INSTANCE* instance, WINDOW_INSTANCE_CB cb);
-int window_instance_set_cb_on_render(WINDOW_INSTANCE* instance, WINDOW_INSTANCE_CB cb, void* cb_param);
+/* Add a on_process_event callback. WINDOW_INSTANCE* will be passed to the callback as parameter1. SDL_Event* will be passed to the callback as parameter2
+ instance:  The window instance
+ cb:        The callback function
+ Returns:   -1 if error, The index of the callback if success */
+int window_instance_add_cb_on_process_event(WINDOW_INSTANCE* instance, WINDOW_INSTANCE_CB_ON_PROCESS_EVENT cb);
 
-/* Create Window Manager. 
+/* Add a on_render callback. 
+ instance:  The window instance
+ cb:        The callback function
+ cb_param1: The callback param1. If NULL, the window instance will be passed to the callback function.
+ cb_param2: The callback param2. If NULL, NULL will be passed to the callback function.
+ Returns:   -1 if error, The index of the callback if success */
+int window_instance_add_cb_on_render(WINDOW_INSTANCE* instance, WINDOW_INSTANCE_CB cb, void* cb_param1, void* cb_param2);
+
+
+/* Set a on_process_event callback of a pre-existing callback. WINDOW_INSTANCE* will be passed to the callback as parameter1. SDL_Event* will be passed to the callback as parameter2
+ instance:  The window instance to add a callback to.
+ index:     The index of the pre-existing callback.
+ cb:        The callback function
+ Returns:   1 if error, 0 if success */
+int window_instance_set_cb_on_process_event(WINDOW_INSTANCE* instance, int index, WINDOW_INSTANCE_CB_ON_PROCESS_EVENT cb);
+
+/* Set a on_render callback of a pre-existing callback.
+ instance:  The window instance to add a callback to.
+ index:     The index of the pre-existing callback.
+ cb:        The callback function
+ cb_param1: The callback param1. If NULL, the window instance will be passed to the callback function.
+ cb_param2: The callback param2. If NULL, NULL will be passed to the callback function.
+ Returns:   1 if error, 0 if success */
+int window_instance_set_cb_on_render(WINDOW_INSTANCE* instance, int index, WINDOW_INSTANCE_CB cb, void* cb_param1, void* cb_param2);
+
+/* Create Window Manager.
+ manager:      The window manager
  window_count: The amount of window instances to allocate memory for. 
- Returns: 1 if error, 0 if success */
-int window_manager_create(uint16_t window_count);
+ Returns:      1 if error, 0 if success */
+int window_manager_create(WINDOW_MANAGER** manager, uint16_t window_count);
 
-/* Destroy Window Manager. */
-void window_manager_destroy(void);
+/* Destroy Window Manager. 
+ manager: The window manager */
+void window_manager_destroy(WINDOW_MANAGER* manager);
 
-/* Process Event. */
-int window_manager_process_event(SDL_Event* e);
+/* Process Event.
+ manager: The window manager
+ e:       The SDL_Event to process 
+ Returns: Non-zero if window_manager wants to quit the program, 0 if not. */
+int window_manager_process_event(WINDOW_MANAGER* manager, SDL_Event* e);
 
-/* Update Window Manager */
-void window_manager_update(void);
+/* Update Window Manager.
+ manager: The window manager */
+void window_manager_update(WINDOW_MANAGER* manager);
 
 #ifdef __cplusplus
 };
